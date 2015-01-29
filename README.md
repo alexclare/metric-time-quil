ca118cc4729d2549e6e88a4ebbfbd50cb562284a
Alexander Clare <alexander.clare@gmail.com>
2015-01-29 03:12:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+metric-time-quil
+================
+**It's time for metric time**...using quil this time. See [metric-time](https://github.com/alexclare/metric-time).
+
+License
+-------
+Copyright © 2015 S714726
+
+Distributed under the GNU General Public License version 3.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject metric-time-quil "0.1.0-SNAPSHOT"
+  :description "metric clock"
+  :url "http://github.com/alexclare/metric-time-quil"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.4"]])
