37c092ca2a32458567582cd7f04717eea71ea9f6
Alexander Clare <alexander.clare@gmail.com>
2015-02-03 05:09:50 -0500

    Simpler

--- src/metric_time_quil/dynamic.clj
+++ src/metric_time_quil/dynamic.clj
@@ -7,21 +7,22 @@
 (defn rotate-fraction [phi] (q/rotate (* phi q/TWO-PI)))
 (defn scale-x [x] (* x (q/width)))
 (defn scale-y [y] (* y (q/height)))
+(defn scaled [fun xa ya xb yb]
+  (fun (scale-x xa) (scale-y ya) (scale-x xb) (scale-y yb)))
 
 (defn clock-base []
   (q/no-fill)
   (q/stroke foreground-color)
 
   (q/stroke-weight 2)
-  (q/ellipse 0 0 (scale-x (/ 1 3)) (scale-y (/ 1 3)))
-  (q/ellipse 0 0 (scale-x (/ 2 3)) (scale-y (/ 2 3)))
+  (scaled q/ellipse 0 0 (/ 1 3) (/ 1 3))
+  (scaled q/ellipse 0 0 (/ 2 3) (/ 2 3))
 
   (q/stroke-weight 1)
   (q/push-matrix)
   (dotimes [i 100]
-    (q/line (scale-x
-             (if (= 0 (mod i 10)) (/ 9 30) (/ 3))) 0
-             (scale-x (/ 11 30)) 0)
+    (scaled q/line (if (= 0 (mod i 10)) (/ 9 30) (/ 3))
+            0 (/ 11 30) 0)
     (rotate-fraction (/ 100)))
   (q/pop-matrix))
 
@@ -31,13 +32,13 @@
   (q/stroke-weight 2)
   (q/push-matrix)
   (rotate-fraction (- (* 100 frac) (Math/floor (* 100 frac))))
-  (q/line 0 0 (scale-x (/ 11 30)) 0)
+  (scaled q/line 0 0 (/ 11 30) 0)
   (q/pop-matrix)
 
   (q/push-matrix)
   (q/stroke-weight 3)
   (rotate-fraction frac)
-  (q/line 0 0 (scale-x (/ 11 30)) 0)
+  (scaled q/line 0 0 (/ 11 30) 0)
   (q/pop-matrix))
 
 
@@ -57,7 +58,7 @@
   ;; this would be a good place to use prior state to clean up intelligently
   (q/stroke background-color)
   (q/fill background-color)
-  (q/rect 0 0 (scale-x 1) (scale-y 1))
+  (scaled q/rect 0 0 1 1)
   (q/translate (scale-x (/ 2)) (scale-y (/ 2)))
   (q/rotate (- q/HALF-PI))
   (clock-base)
