3e24ca964769b7a9280845b1ff7bd31c7d56c843
Alexander Clare <alexander.clare@gmail.com>
2015-02-03 05:01:28 -0500

    Converted from CLJS

--- src/metric_time_quil/dynamic.clj
+++ src/metric_time_quil/dynamic.clj
@@ -1,8 +1,64 @@
 (ns metric-time-quil.dynamic
   (:require [quil.core :as q]))
 
-(defn setup [])
+(def background-color 255)
+(def foreground-color 0)
 
-(defn update [state])
+(defn rotate-fraction [phi] (q/rotate (* phi q/TWO-PI)))
+(defn scale-x [x] (* x (q/width)))
+(defn scale-y [y] (* y (q/height)))
 
-(defn draw [state])
+(defn clock-base []
+  (q/no-fill)
+  (q/stroke foreground-color)
+
+  (q/stroke-weight 2)
+  (q/ellipse 0 0 (scale-x (/ 1 3)) (scale-y (/ 1 3)))
+  (q/ellipse 0 0 (scale-x (/ 2 3)) (scale-y (/ 2 3)))
+
+  (q/stroke-weight 1)
+  (q/push-matrix)
+  (dotimes [i 100]
+    (q/line (scale-x
+             (if (= 0 (mod i 10)) (/ 9 30) (/ 3))) 0
+             (scale-x (/ 11 30)) 0)
+    (rotate-fraction (/ 100)))
+  (q/pop-matrix))
+
+
+(defn clock-hands [frac]
+  (q/stroke foreground-color)
+  (q/stroke-weight 2)
+  (q/push-matrix)
+  (rotate-fraction (- (* 100 frac) (Math/floor (* 100 frac))))
+  (q/line 0 0 (scale-x (/ 11 30)) 0)
+  (q/pop-matrix)
+
+  (q/push-matrix)
+  (q/stroke-weight 3)
+  (rotate-fraction frac)
+  (q/line 0 0 (scale-x (/ 11 30)) 0)
+  (q/pop-matrix))
+
+
+(defn setup []
+  (q/frame-rate 30)
+  {:fraction 0.0})
+
+(defn update [state]
+  {:fraction (-> (q/hour)
+                 (* 60)
+                 (+ (q/minute))
+                 (* 60)
+                 (+ (q/seconds))
+                 (/ 86400))})
+
+(defn draw [state]
+  ;; this would be a good place to use prior state to clean up intelligently
+  (q/stroke background-color)
+  (q/fill background-color)
+  (q/rect 0 0 (scale-x 1) (scale-y 1))
+  (q/translate (scale-x (/ 2)) (scale-y (/ 2)))
+  (q/rotate (- q/HALF-PI))
+  (clock-base)
+  (clock-hands (:fraction state)))
